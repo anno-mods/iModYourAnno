2136d9fc5bf4df44bb6e0f03d1430f4987823fd6
taubenangriff <montag.lennard@gmail.com>
2021-12-28 14:05:37 +0100

    Add new ValueConverters

---
+++ ModManager/ValueConverters/DlcIconConverter.cs
@@ -0,0 +1,47 @@
+﻿using Imya.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace Imya.UI.ValueConverters
+{
+    [ValueConversion(typeof(DlcId), typeof(String))]
+    internal class DlcIconConverter : IValueConverter
+    {
+        private Dictionary<DlcId, String> DlcIconMapping = new Dictionary<DlcId, String>
+        {
+            { DlcId.SunkenTreasures, "dlc_cape"},
+            { DlcId.Botanica, "dlc_botanica" },
+            { DlcId.ThePassage,"dlc_passage" },
+            { DlcId.SeatOfPower, "dlc_palace" },
+            { DlcId.BrightHarvest, "dlc_brightharvest" },
+            { DlcId.LandOfLions, "dlc_lol" },
+            { DlcId.Docklands, "dlc_docklands" },
+            { DlcId.Tourism, "dlc_tourists" },
+            { DlcId.Highlife, "dlc_highlife" },
+            { DlcId.Anarchist, "dlc_anarchist" }
+        };
+
+        public object Convert(object value, Type TargetType, object parameter, CultureInfo Culture)
+        {
+            var dlc = (DlcId)value;
+            return "\\"+ Path.Combine("resources", "dlc_icons", GetDlcId(dlc) + ".png");
+        }
+
+        private String GetDlcId(DlcId id)
+        {
+            if (DlcIconMapping.TryGetValue(id, out String? Filepath))
+            {
+                return Filepath;
+            }
+            else return "dlc_anarchist";
+        }
+
+        public object ConvertBack(object value, Type TargetType, object parameter, CultureInfo Culture)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
---
+++ ModManager/ValueConverters/DlcTextConverter.cs
@@ -0,0 +1,60 @@
+﻿using Imya.Enums;
+using Imya.Models;
+using Imya.Utils;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace Imya.UI.ValueConverters
+{
+    /// <summary>
+    /// A converter that maps DLC IDs to LocalizedTexts
+    /// </summary>
+    [ValueConversion(typeof(DlcId), typeof(LocalizedText))]
+    internal class DlcTextConverter : IValueConverter
+    {
+        private TextManager TextManager = TextManager.Instance;
+
+        private Dictionary<DlcId, String> DlcTextMapping = new Dictionary<DlcId, String>
+        {
+            { DlcId.SunkenTreasures, "DLC_SUNKENTREASURES"},
+            { DlcId.Botanica, "DLC_BOTANICA" },
+            { DlcId.ThePassage,"DLC_PASSAGE" },
+            { DlcId.SeatOfPower, "DLC_SEATOFPOWER" },
+            { DlcId.BrightHarvest, "DLC_BRIGHTHARVEST" },
+            { DlcId.LandOfLions, "DLC_LANDOFLIONS" },
+            { DlcId.Docklands, "DLC_DOCKLANDS" },
+            { DlcId.Tourism, "DLC_TOURISTSEASON" },
+            { DlcId.Highlife, "DLC_HIGHLIFE" },
+            { DlcId.Christmas, "DLC_CHRISTMAS" },
+            { DlcId.AmusementPark, "DLC_AMUSEMENTPARK" },
+            { DlcId.CityLife, "DLC_CITYLIFE" },
+            { DlcId.VehicleSkins, "DLC_VEHICLESKINS" },
+            { DlcId.PedestrianZone, "DLC_PEDESTRIANZONE" },
+            { DlcId.VibrantCity, "DLC_VIBRANTCITY" },
+            { DlcId.Anarchist, "DLC_ANARCHIST" }
+        };
+
+        public object Convert(object value, Type TargetType, object parameter, CultureInfo Culture)
+        {
+            var dlc = (DlcId)value;
+            return TextManager[GetDlcText(dlc)];
+        }
+
+        private String GetDlcText(DlcId id)
+        {
+            if (DlcTextMapping.TryGetValue(id, out String? Filepath))
+            {
+                return Filepath;
+            }
+            else return "NO_TEXT";
+        }
+
+        public object ConvertBack(object value, Type TargetType, object parameter, CultureInfo Culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
