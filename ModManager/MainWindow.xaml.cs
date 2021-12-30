78e1a3159735364cee1f4d16c111a242f99458e3
taubenangriff <montag.lennard@gmail.com>
2021-12-30 13:21:31 +0100

    Update Handling of Main Window:
    Main Window TabControl binds to an Index Property in the Dashboard

--- ModManager/MainWindow.xaml.cs
+++ ModManager/MainWindow.xaml.cs
@@ -9,29 +9,22 @@ namespace Imya.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SettingsManager SettingsManager { get; } = SettingsManager.Instance;
+
         public MainWindow()
         {
             InitializeComponent();
             SetUpEmbeddedConsole();
-            Dashboard.MainViewChanged += ChangeMainWindow;
             Console.WriteLine("Modders Are gonna take over the World!!!");
-
+            DataContext = this;
             MinHeight = Settings.Default.MIN_WINDOW_HEIGHT;
             MinWidth = Settings.Default.MIN_WINDOW_WIDTH;
+
         }
 
         public void SetUpEmbeddedConsole()
         {
             Console.SetOut(new EmbeddedConsole(ConsoleLogTextBox.ConsoleOut, this));
-            
-        }
-
-        private void ChangeMainWindow(int index)
-        {
-            if (index >= 0 && index <= 1)
-            {
-                WindowViewControl.SelectedIndex = index;
-            }
         }
     }
 }
