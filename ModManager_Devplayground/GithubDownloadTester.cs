64764ff767c5c4201f408bc15458ef59f90596aa
taubenangriff <montag.lennard@gmail.com>
2022-02-02 11:44:21 +0100

    Add Developer Playground project

---
+++ ModManager_Devplayground/GithubDownloadTester.cs
@@ -0,0 +1,16 @@
+﻿using Imya.GithubIntegration;
+
+internal class GithubDevTester
+{
+    internal async static void DownloadModloader()
+    {
+        GithubDownloader Downloader = new GithubDownloader("fuck");
+
+        File.Delete("fuck/loader.zip");
+
+        await Downloader.DownloadReleaseAsync(new GithubRepoInfo { Name = "anno1800-mod-loader", Owner = "xforce" }, "loader.zip");
+
+        var file = new FileInfo("fuck/loader.zip");
+        Console.WriteLine($"Download Success: { file.Exists && file.Length != 0 }");
+    }
+}
---
+++ ModManager_Devplayground/Program.cs
@@ -0,0 +1,10 @@
+﻿using Imya;
+using Imya.GithubIntegration;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        GithubDevTester.DownloadModloader();
+    }
+}
