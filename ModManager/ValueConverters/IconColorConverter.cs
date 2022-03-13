dea0617a34f12fe23af298d6aeac8f2cd97ab5db
taubenangriff <montag.lennard@gmail.com>
2022-03-13 18:37:05 +0100

    make mod activation icon colors adjustable through themes

--- ModManager/ValueConverters/IconColorConverter.cs
+++ ModManager/ValueConverters/IconColorConverter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Imya.UI.ValueConverters
 {
@@ -10,13 +12,30 @@ namespace Imya.UI.ValueConverters
         public object Convert(object value, Type TargetType, object parameter, CultureInfo Culture)
         {
             bool b = (bool)value;
+
+            try
+            {
+                SolidColorBrush? ColorActive = Application.Current.Resources["ModActiveColorBrush"] as SolidColorBrush;
+                SolidColorBrush? ColorInactive = Application.Current.Resources["ModInactiveColorBrush"] as SolidColorBrush;
+
+                if (ColorActive is not null 
+                    && ColorInactive is not null)
+                { 
+                    return b ? ColorActive : ColorInactive;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            //fallback
             return b ? "Green" : "Gray";
         }
 
         public object ConvertBack(object value, Type TargetType, object parameter, CultureInfo Culture)
         {
-            string strValue = value as string;
-            return strValue.Equals("Green");
+            throw new NotImplementedException("We don't need to convert from colors :)");
         }
     }
 }
