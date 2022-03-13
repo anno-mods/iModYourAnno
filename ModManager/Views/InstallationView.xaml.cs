6daea8ea3cde71925bbc656f00d4291f463f77c0
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2022-03-13 18:25:49 +0100

    install: add simple zip install

---
+++ ModManager/Views/InstallationView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Imya.Utils;
+
+namespace Imya.UI.Views
+{
+    /// <summary>
+    /// Interaktionslogik für DummyControl.xaml
+    /// </summary>
+    public partial class DummyControl : UserControl
+    {
+        public DummyControl()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnInstallFromZip(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.OpenFileDialog
+            {
+                Filter = "Zip Archives (*.zip)|*.zip",
+                RestoreDirectory = true, // TODO keep location separate from game path dialog, it's annoying!
+                Multiselect = true
+            };
+            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            var modDirectories = new List<ModInstaller.ModDirectoryTodo>();
+
+            foreach (var filePath in dialog.FileNames)
+            {
+                Console.WriteLine($"Extract zip: {filePath}");
+                var result = await ModInstaller.PrepareInstallZipAsync(filePath, Path.Combine(Directory.GetCurrentDirectory(), Properties.Settings.Default.DownloadDir));
+                if (result != null)
+                    modDirectories.Add(result);
+            }
+
+            // TODO this is the chance to select which mods to install
+            // for now let's just go with installing all
+
+            foreach (var modDirectory in modDirectories)
+            {
+                Console.WriteLine($"Install zip: {modDirectory.ExtractedBase}");
+                await ModInstaller.FinalizeInstallAsync(modDirectory, GameSetupManager.Instance.GetModDirectory());
+            }
+        }
+    }
+}
---
+++ ModManager_Classes/Utils/ModInstaller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Imya.GithubIntegration;
+
+namespace Imya.Utils
+{
+    /// <summary>
+    /// Install mods from zip file.
+    /// </summary>
+    public class ModInstaller
+    {
+        public class ModDirectoryTodo
+        {
+            // this will be very much like ModDirectoryManager, hence transform the manager into a reusable class
+            public string ExtractedBase { get; init; } = "";
+            public string[] ModFolders { get; init; } = Array.Empty<string>();
+        }
+
+        public static async Task<ModDirectoryTodo?> PrepareInstallZipAsync(string zipFilePath, string tempDir)
+        {
+            // TODO issue handling
+            if (!Directory.Exists(tempDir) || !File.Exists(zipFilePath)) return null;
+
+            string extractTarget = Path.Combine(tempDir, Path.GetFileNameWithoutExtension(zipFilePath));
+            
+            if (Directory.Exists(extractTarget))
+                Directory.Delete(extractTarget, true);
+
+            await Task.Run(() => ZipFile.ExtractToDirectory(zipFilePath, extractTarget, true));
+
+            var modFolders = Directory.GetDirectories(extractTarget).Select(x => Path.GetRelativePath(extractTarget, x)).ToArray();
+            return new ModDirectoryTodo() { ExtractedBase = extractTarget, ModFolders = modFolders };
+        }
+
+        public static async Task FinalizeInstallAsync(ModDirectoryTodo modDirectory, string modFolderPath)
+        {
+            // TODO issue handling
+            await Task.Run(() => {
+                foreach (var folder in modDirectory.ModFolders)
+                {
+                    var targetMod = Path.Combine(modFolderPath, folder);
+                    // TODO move with overwrite instead of mod deletion
+                    Directory.Delete(targetMod, true);
+                    Directory.Move(Path.Combine(modDirectory.ExtractedBase, folder), targetMod);
+                }
+
+                Directory.Delete(modDirectory.ExtractedBase, true);
+            });
+        }
+    }
+}
