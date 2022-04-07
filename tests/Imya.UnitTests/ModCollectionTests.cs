c32dbb05f1aeb0bb01d925aab8993875e2fa5a30
taubenangriff <montag.lennard@gmail.com>
2022-04-08 00:26:44 +0200

    Ensure the text manager exists on the tests

--- tests/Imya.UnitTests/ModCollectionTests.cs
+++ tests/Imya.UnitTests/ModCollectionTests.cs
@@ -182,6 +182,8 @@ namespace Imya.UnitTests
         [Fact]
         public void MoveInto_UpdateWithDifferentFolder()
         {
+            TextManager t = new TextManager(); 
+
             DirectoryEx.EnsureDeleted("tmp");
 
             // create obsolete target
@@ -263,6 +265,8 @@ namespace Imya.UnitTests
         [Fact]
         public void MoveInto_SameFolderDifferentModID()
         {
+            TextManager t = new TextManager();
+
             DirectoryEx.EnsureDeleted("tmp");
 
             // create target
