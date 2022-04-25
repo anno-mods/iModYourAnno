b239a4670929c5d9843a64fcdd114a9127e2f0d1
taubenangriff <montag.lennard@gmail.com>
2022-04-25 15:34:46 +0200

    Update dev playground

--- ModManager_Devplayground/GithubDownloadTester.cs
+++ ModManager_Devplayground/GithubDownloadTester.cs
@@ -1,23 +1,29 @@
 ﻿using Imya.GithubIntegration;
+using Imya.Models.Installation;
 using Imya.Utils;
 
 internal class GithubDevTester
 {
-    internal async static void DownloadModloader()
+    internal async static Task DownloadSpice()
     {
         GithubDownloader Downloader = new GithubDownloader("fuck");
 
         File.Delete("fuck/loader.zip");
 
-        await Downloader.DownloadReleaseAsync(new GithubRepoInfo { Name = "Spice-it-Up", Owner = "anno-mods" }, "Spice-it-Up-3-0-6.zip");
+        await Downloader.DownloadReleaseAsync(new GithubRepoInfo { Name = "Spice-it-Up", Owner = "anno-mods" }, "Spice-it-Up.zip", new ModloaderInstallation());
 
-        var file = new FileInfo("fuck/loader.zip");
+        var file = new FileInfo("fuck/Spice-it-Up.zip");
         Console.WriteLine($"Download Success: { file.Exists && file.Length != 0 }");
     }
 
-    internal static void DownloadModloader2()
+    internal async static Task DownloadModloader2()
     {
         ModLoaderInstaller installation = new ModLoaderInstaller("", "imya_temp");
-        installation.InstallAsync();
+        await installation.InstallAsync(new ModloaderInstallation() );
+    }
+
+    internal class ModloaderInstallation : Installation
+    {
+
     }
 }
