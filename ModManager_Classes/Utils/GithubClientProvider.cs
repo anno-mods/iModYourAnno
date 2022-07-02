8d3390e89a3a4e70ac4343f88b1b36164d018f26
taubenangriff <montag.lennard@gmail.com>
2022-07-03 00:03:09 +0200

    we need a single github client across the application

---
+++ ModManager_Classes/Utils/GithubClientProvider.cs
@@ -0,0 +1,14 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imya.Utils
+{
+    internal class GithubClientProvider
+    {
+        internal static GitHubClient Client { get; } = new GitHubClient(new ProductHeaderValue("iModYourAnno"));
+    }
+}
