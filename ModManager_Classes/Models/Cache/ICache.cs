319bc4bdfccef3b31f27d716f28278abb961bf58
taubenangriff <montag.lennard@gmail.com>
2022-07-02 23:59:38 +0200

    Add Caching infrastructure

---
+++ ModManager_Classes/Models/Cache/ICache.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imya.Models.Cache
+{
+    public interface ICache<TKey, TResult>
+    {
+        Task<TResult> GetOrCreateAsync(TKey key, Func<ICacheEntry, Task<TResult>> factory);
+
+        TResult GetOrCreate(TKey key, Func<ICacheEntry, TResult> factory);
+    }
+}
