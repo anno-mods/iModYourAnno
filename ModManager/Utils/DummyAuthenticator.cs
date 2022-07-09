d19672c41911110fb9017a415be237459913ead5
taubenangriff <montag.lennard@gmail.com>
2022-07-09 12:33:34 +0200

    add authentication logic

---
+++ ModManager/Utils/DummyAuthenticator.cs
@@ -0,0 +1,24 @@
+﻿using Imya.Utils;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imya.UI.Utils
+{
+    internal class DummyAuthenticator : IAuthenticator
+    {
+        public async Task RunAuthenticate(GitHubClient Client)
+        {
+            Console.WriteLine("Authentication is not supported currently!");
+        }
+
+        public IAuthenticator.Secrets LoadSecrets(String Filename)
+        {
+            Console.WriteLine("Authentication is not supported currently!");
+            return new IAuthenticator.Secrets();
+        }
+    }
+}
---
+++ ModManager_Classes/Utils/IAuthenticator.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imya.Utils
+{
+    public interface IAuthenticator
+    {
+        public Task RunAuthenticate(GitHubClient Client);
+
+        public Secrets LoadSecrets(String Filename);
+
+        public struct Secrets
+        {
+            public String ClientId { get; set; }
+            public String ClientSecret { get; set; }
+        }
+    }
+}
