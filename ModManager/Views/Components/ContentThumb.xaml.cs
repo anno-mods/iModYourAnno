41e1d70973ef04d6fa7b76248a6da553691ebf8d
taubenangriff <montag.lennard@gmail.com>
2022-07-24 18:51:50 +0200

    Add Slider with Support for Content

---
+++ ModManager/Views/Components/ContentThumb.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Imya.UI.Views.Components
+{
+    /// <summary>
+    /// Interaktionslogik für ContentSlider.xaml
+    /// </summary>
+    public class ContentThumbBase : Thumb { }
+
+    public partial class ContentThumb : ContentThumbBase
+    {
+        public static readonly DependencyProperty ContentProperty =
+              DependencyProperty.Register("Content", typeof(object),
+            typeof(ContentThumb), new UIPropertyMetadata(String.Empty));
+
+        public ContentThumb() : base()
+        {
+
+        }
+
+        public String Content
+        {
+            get
+            {
+                return (String) GetValue(ContentProperty);
+            }
+            set
+            {
+                SetValue(ContentProperty, value);
+            }
+        }
+    }
+}
