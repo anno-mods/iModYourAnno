704b597311258d7a48213eb47c7c9da3a54c1794
taubenangriff <montag.lennard@gmail.com>
2022-07-25 23:38:08 +0200

    remove username and shit from deviceflowauthenticator

--- ModManager/Utils/DeviceFlowAuthenticator.cs
+++ ModManager/Utils/DeviceFlowAuthenticator.cs
@@ -10,24 +10,12 @@ using System.Threading.Tasks;
 
 namespace Imya.UI.Utils
 {
-    public class DeviceFlowAuthenticator : PropertyChangedNotifier, IAuthenticator
+    public class DeviceFlowAuthenticator : IAuthenticator
     {
         private String ClientID = "37a11ee844d5eea41346";
 
         public event IAuthenticator.PopupRequestedEventHandler UserCodeReceived = delegate { };
         public event IAuthenticator.AuthenticatedEventHandler AuthenticationSuccess = delegate { };
-
-        public bool IsAuthenticated { 
-            get => _isAuthenticated;
-            set => SetProperty(ref _isAuthenticated, value);
-        }
-        private bool _isAuthenticated = false;
-
-        public String? AuthenticatedUser { 
-            get => _authenticatedUser;
-            set => SetProperty(ref _authenticatedUser, value);
-        }
-        private String? _authenticatedUser;
 
         public async Task RunAuthenticate(GitHubClient Client)
         {
@@ -42,10 +30,6 @@ namespace Imya.UI.Utils
             {
                 var credentials = new Credentials(valid_token.AccessToken);
                 Client.Credentials = credentials;
-
-                var user = (await Client.User.Current());
-                IsAuthenticated = true;
-                AuthenticatedUser = user.Login;
                 AuthenticationSuccess?.Invoke();
             }
         }
