7036cc80100fe6d629d4b7d90598715d30d59b4f
taubenangriff <montag.lennard@gmail.com>
2022-08-26 21:21:22 +0200

    Replace Custom Download with bezzad/Downloader

--- ModManager_Classes/GithubIntegration/Download/GithubDownloader.cs
+++ ModManager_Classes/GithubIntegration/Download/GithubDownloader.cs
@@ -9,6 +9,8 @@ using Imya.Models.Options;
 using Imya.Utils;
 using Octokit;
 
+using Downloader;
+
 namespace Imya.GithubIntegration.Download
 {
     public struct DownloadResult
@@ -40,20 +42,35 @@ namespace Imya.GithubIntegration.Download
         /// <returns>the Filepath where the release has been downloaded to.</returns>
         private async Task<DownloadResult> DownloadReleaseAssetAsync(ReleaseAsset releaseAsset, IProgress<float>? progress = null)
         {
-            var downloadURL = releaseAsset.BrowserDownloadUrl;
-            if (downloadURL is null) throw new InstallationException("No matching release found");
+            if (releaseAsset.BrowserDownloadUrl is null) throw new InstallationException("No matching release found");
 
             String TargetFilename = Path.Combine(Options.DownloadDirectory, releaseAsset.Name);
 
             try
             {
-                using (HttpClient DownloadClient = new HttpClient())
-                using (Stream targetStream = File.Create(TargetFilename))
+                //add configuration options like download limit later
+                var downloadOpt = new DownloadConfiguration()
                 {
-                    DownloadClient.Timeout = Options.Timeout;
-                    await DownloadClient.DownloadAsync(downloadURL, targetStream, Options.DownloadBufferSize, Options.Timeout, progress);
-                    return new DownloadResult { DownloadSuccessful = true, DownloadDestination = TargetFilename };
+                    ChunkCount = 4,
+                    ParallelDownload = true,
+                    TempDirectory = Options.DownloadDirectory
+                };
+
+                IDownload download = DownloadBuilder.New()
+                    .WithUrl(releaseAsset.BrowserDownloadUrl)
+                    .WithFileLocation(TargetFilename)
+                    .WithConfiguration(downloadOpt)
+                    .Build();
+
+                if (progress is not null)
+                {
+                    download.DownloadProgressChanged += (sender, e) => {
+                        progress.Report((float)e.ProgressPercentage / 100);
+                    };
                 }
+
+                await download.StartAsync();
+                return new DownloadResult { DownloadSuccessful = true, DownloadDestination = TargetFilename };
             }
             catch (Exception e)
             {
@@ -72,58 +89,6 @@ namespace Imya.GithubIntegration.Download
             }
 
             return await DownloadReleaseAssetAsync(releaseAsset, progress);
-        }
-    }
-
-    public static class HttpClientExtensions
-    {
-        public static async Task DownloadAsync(this HttpClient client,
-            String RequestUri, 
-            Stream destination,
-            int BufferSize,
-            TimeSpan Timeout,
-            IProgress<float>? progress = null, 
-            CancellationToken cancellationToken = default)
-        {
-            using (var response = await client.GetAsync(RequestUri, HttpCompletionOption.ResponseHeadersRead))
-            using (var ContentStream = await response.Content.ReadAsStreamAsync())
-            {
-                var contentLength = response.Content.Headers.ContentLength ?? 0;
-                await ContentStream.CopyToAsync(destination, BufferSize, Timeout, progress, cancellationToken, contentLength);
-            }
-        }
-    }
-
-    public static class StreamExtensions
-    {
-        public static async Task CopyToAsync(this Stream source, 
-            Stream destination, 
-            int bufferSize, 
-            TimeSpan Timeout,
-            IProgress<float>? progress = null, 
-            CancellationToken cancellationToken = default, 
-            long totalBytes = 1)
-        {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-            if (!source.CanRead)
-                throw new ArgumentException("Has to be readable", nameof(source));
-            if (destination == null)
-                throw new ArgumentNullException(nameof(destination));
-            if (!destination.CanWrite)
-                throw new ArgumentException("Has to be writable", nameof(destination));
-            if (bufferSize < 0)
-                throw new ArgumentOutOfRangeException(nameof(bufferSize));
-
-            var buffer = new byte[bufferSize];
-            long totalBytesRead = 0;
-            int bytesRead;
-            while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).TimeoutAfter(Timeout)) != 0)
-            {
-                await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken);
-                totalBytesRead += bytesRead;
-                progress?.Report((float)totalBytesRead / totalBytes);
-            }
         }
     }
 }
