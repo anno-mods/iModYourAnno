fcf2f8633ddf171abe1fa68e00477f9fadce09aa
Jakob Harder <12190313+jakobharder@users.noreply.github.com>
2022-08-28 09:56:16 +0200

    hide tweak tooltip icon if there's no tooltip

---
+++ ModManager/ValueConverters/IsEmptyConverters.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Imya.UI.ValueConverters
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    internal sealed class IsEmptyToBool : IValueConverter
+    {
+        public bool OnEmpty { get; set; }
+
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (value is not string str)
+                return OnEmpty;
+            return string.IsNullOrWhiteSpace(str) ? OnEmpty : !OnEmpty;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            // be silent
+            return string.Empty;
+        }
+    }
+
+    [ValueConversion(typeof(string), typeof(Visibility))]
+    internal sealed class IsEmptyToVisibility : IValueConverter
+    {
+        public Visibility OnEmpty { get; set; }
+        public Visibility OnElse { get; set; }
+
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (value is not string str)
+                return OnEmpty;
+            return string.IsNullOrWhiteSpace(str) ? OnEmpty : OnElse;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            // be silent
+            return string.Empty;
+        }
+    }
+}
