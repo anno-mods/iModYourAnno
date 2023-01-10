5d20b0ed69f1bc01bc10e95b57fa6b084a692e2b
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-01-10 22:54:32 +0100

    add image strategy

---
+++ ModManager_Classes/GithubIntegration/IModImageStrategy.cs
@@ -0,0 +1,14 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imya.GithubIntegration
+{
+    public interface IModImageStrategy
+    {
+        Task<string?> GetImageUrlAsync(GithubRepoInfo repoInfo);
+    }
+}
