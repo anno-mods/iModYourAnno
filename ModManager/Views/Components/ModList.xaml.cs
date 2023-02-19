bae2b6d525c6d8e460c697d7c913e3b32f776200
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-02-19 15:10:30 +0100

    call the collection instead

--- ModManager/Views/Components/ModList.xaml.cs
+++ ModManager/Views/Components/ModList.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace Imya.UI.Components
@@ -60,15 +61,17 @@ namespace Imya.UI.Components
 
         public async void ActivateSelection()
         {
-            foreach (Mod mod in ListBox_ModList.SelectedItems.OfType<BindableMod>().Select(x => x.Model).ToArray())
-                await mod.ChangeActivationAsync(true);
+            var selected = ListBox_ModList.SelectedItems.OfType<BindableMod>().Select(x => x.Model).ToArray();
+            await Mods.WrappedCollection.ChangeActivationAsync(selected, true);
+
             OnSelectionChanged(); 
         }
 
         public async void DeactivateSelection()
         {
-            foreach (Mod mod in ListBox_ModList.SelectedItems.OfType<BindableMod>().Select(x => x.Model).ToArray())
-                await mod.ChangeActivationAsync(false);
+            var selected = ListBox_ModList.SelectedItems.OfType<BindableMod>().Select(x => x.Model).ToArray();
+            await Mods.WrappedCollection.ChangeActivationAsync(selected, false);
+
             OnSelectionChanged();
         }
 
