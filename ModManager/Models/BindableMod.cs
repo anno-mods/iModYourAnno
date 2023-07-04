d4da1721496538e549af27a7cbc505dcb6e12aa9
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-07-05 00:21:03 +0200

    Add all distinct submods to the UI binding

--- ModManager/Models/BindableMod.cs
+++ ModManager/Models/BindableMod.cs
@@ -1,6 +1,9 @@
 ﻿using Imya.Models;
 using Imya.Models.Attributes;
 using Imya.Models.Mods;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using System.Windows.Threading;
 
 namespace Imya.UI.Models
@@ -15,11 +18,18 @@ namespace Imya.UI.Models
         public bool IsRemoved => Model.IsRemoved;
         public IText Name => Model.Name;
         public IText Category => Model.Category;
+
+        public BindableCollection<Mod> SubMods { get; private set; }
+
+        public bool HasSubmods => Model.HasSubmods;
+
         public BindableCollection<IAttribute> Attributes { get; private set; }
 
         public BindableMod(Mod mod, DispatcherObject context) : base(mod, context)
         {
             Attributes = new(mod.Attributes, context);
+            var distinct = mod.SubMods.DistinctBy(x => (x.ModID, x.Version)).ToList().AsReadOnly();
+            SubMods = new(distinct, context);
         }      
     }
 }
